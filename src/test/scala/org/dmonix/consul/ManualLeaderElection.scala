538d3b208284cf3fa0e9e9b487adfde030774f63
Peter Nerg <peter.nerg@gmail.com>
2018-10-26 17:10:42 +0200

    added manual playground for leader election

---
+++ src/test/scala/org/dmonix/consul/ManualLeaderElection.scala
@@ -0,0 +1,30 @@
+package org.dmonix.consul
+
+import java.util.concurrent.Semaphore
+
+/**
+  * Simple app to illustrate the leadership election process.   
+  * Start multiple instances to have more candidates for the election process.  
+  * Requires Consul to be litening to localhost:8500 (see README.md for example to start Consul)
+  * @author Peter Nerg
+  */
+object ManualLeaderElection extends App {
+  private val semaphore = new Semaphore(0)
+  
+  //simple observer that just prints election changes
+  private val observer = new ElectionObserver {
+    override def elected(): Unit = println("Elected")
+    override def unElected(): Unit = println("UnElected")
+  }
+  
+  private val candidate = LeaderElection.joinLeaderElection(ConsulHost("localhost"), "example-group", None, Option(observer)) get
+  
+  //catches shutdown of the app and makes the candidate leave the election process
+  sys.addShutdownHook {
+    candidate.leave()
+    semaphore.release()
+  }
+  
+  //hold here for the lifetime of the app
+  semaphore.acquire()
+}
