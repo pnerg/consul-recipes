87b740f275a84af4ce225e4b55d54a868bc1f85e
Peter Nerg <peter.nerg@gmail.com>
2020-02-06 13:36:14 +0100

    refactored the key/value management in the ConsulSim into its own class

--- consul-sim/src/main/scala/org/dmonix/consul/ConsulSim.scala
+++ consul-sim/src/main/scala/org/dmonix/consul/ConsulSim.scala
@@ -15,9 +15,6 @@
   */
 package org.dmonix.consul
 
-import java.util.concurrent.atomic.AtomicInteger
-import java.util.concurrent.{TimeUnit, Semaphore => jSemaphore}
-
 import akka.actor.{ActorSystem, Terminated}
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.Http.ServerBinding
@@ -31,20 +28,14 @@ import org.slf4j.LoggerFactory
 import spray.json._
 
 import scala.collection._
-import scala.concurrent.duration.{DurationInt, FiniteDuration}
+import scala.concurrent.duration.DurationInt
 import scala.concurrent.{Await, Future}
 
 
 
 object ConsulSim  {
-  private case class Blocker(index:Int, semaphore: jSemaphore) {
-    def releaseIfIndexReached(mi:Int):Unit = if(mi >= index)semaphore.release()
-  }
-
   def apply() = new ConsulSim()
 }
-
-import ConsulSim._
 
 /**
   * Simulates Consul
@@ -63,19 +54,11 @@ class ConsulSim {
   private var server: Option[ServerBinding] = None
 
   /** The internal session storage */
-  val sessionStorage = new SessionStorage()
-  
-  private val creationCounter = new AtomicInteger(0)
-  private val modificationCounter = new AtomicInteger(0)
-  
-  private val keyValues = mutable.Map[String, KeyValue]()
-  private val blockers = mutable.Map[String, Seq[Blocker]]()
+  val sessionStorage = SessionStorage()
 
-  //============================================================
-  //
-  // Public functions
-  //
-  //============================================================
+  /** The internal key/value storage */
+  val keyValueStorage = KeyValueStorage()
+  
   /**
     * Starts the simulator on the designated port
     * @param port The port to listen to, defaults to 0 i.e. chosen by the host
@@ -108,11 +91,6 @@ class ConsulSim {
     */
   def consulHost:Option[ConsulHost] = server.map(b => ConsulHost("localhost", b.localAddress.getPort))
 
-  //============================================================
-  //
-  // Internal/private functions
-  //
-  //============================================================
   /**
     * ==============================
     * Route for managing the various session related requests sent to the simulator
@@ -175,11 +153,7 @@ class ConsulSim {
                 complete(StatusCodes.InternalServerError, s"invalid session '$id'")
               case _ =>
                 val newValue = entity.filterNot(_.isEmpty)
-                val kv = keyValues
-                  .get(key)
-                  .map(_.copy(value = newValue))
-                  .getOrElse(KeyValue(createIndex = nextCreationIndex, modifyIndex = 0, lockIndex = 0, key = key, session = None, value = newValue))
-                val result = attemptSetKey(kv, cas.map(_.toInt), acquire, release)
+                val result = keyValueStorage.createOrUpdate(key, newValue, cas.map(_.toInt), acquire, release)
                 complete(HttpEntity(ContentTypes.`application/json`, result.toString))
             }
           }
@@ -191,13 +165,13 @@ class ConsulSim {
             val waitDuration = wait.map(_.asFiniteDuration).filterNot(_ == zeroDuration) getOrElse defaultDuration
             val modifyIndex = index.map(_.toInt) getOrElse 0
             logger.debug(s"Attempting to read [$key] with index [$modifyIndex] wait [$waitDuration] and recurse [$recurse]")
-            readKey(key, modifyIndex, waitDuration) match {
+            keyValueStorage.readKey(key, modifyIndex, waitDuration) match {
               //non-recursive call return the found key
               case Some(kv) if recurse.isEmpty =>
                 complete(HttpEntity(ContentTypes.`application/json`, Seq(kv).toJson.prettyPrint))
               //recursive call, return all keys on the requested path
               case _ if recurse.isDefined =>
-                val res = keyValues.filterKeys(_.startsWith(key)).values.toSeq
+                val res = keyValueStorage.getKeysForPath(key)
                 complete(HttpEntity(ContentTypes.`application/json`, res.toJson.prettyPrint))
               //no such key
               case _ =>
@@ -209,69 +183,11 @@ class ConsulSim {
         delete {
           parameters('cas ?, 'recurse.?) { (cas, recurse) =>
             val recursive = recurse getOrElse false //TODO implement recursive delete
-            keyValues.remove(key).foreach{kv =>
-              blockers.get(kv.key).foreach(_.foreach(_.semaphore.release()))
-            }
+            keyValueStorage.removeKey(key)
             complete(HttpEntity(ContentTypes.`application/json`, "true"))
           }
         }
     }
-
-  private def nextCreationIndex =  creationCounter.getAndIncrement()
-  private def nextModificationIndex =  modificationCounter.getAndIncrement()
-  
-  private def attemptSetKey(kv: KeyValue, cas:Option[Int], acquire:Option[String], release:Option[String]): Boolean = {
-    val passedCAS = cas.map(_ == kv.modifyIndex) getOrElse true
-    def isUnlocked:Boolean = kv.session.isEmpty
-    def isLockOwner(sessionID:String): Boolean = kv.session.map(_ == sessionID) getOrElse false 
-
-    val res = (acquire, release) match {
-      //compare-and-set failed, bail out
-      case _ if !passedCAS => None
-      //trying to take lock with no owner
-      case (Some(_), None) if isUnlocked => Some(kv.copy(modifyIndex = nextModificationIndex, session = acquire, lockIndex = kv.lockIndex+1))
-      //trying to take lock whilst owning it       
-      case (Some(id), None) if isLockOwner(id) => Some(kv.copy(modifyIndex = nextModificationIndex, session = acquire))
-      //trying to take lock whilst NOT owning it       
-      case (Some(id), None) if !isLockOwner(id) => None
-      //trying to release lock with no owner   
-      case (None, Some(_)) if isUnlocked => None
-      //trying to release lock whilst owning it       
-      case (None, Some(id)) if isLockOwner(id) => Some(kv.copy(modifyIndex = nextModificationIndex, session = None))
-      //trying to release lock whilst NOT owning it       
-      case (None, Some(id)) if !isLockOwner(id) => None
-      //neither 'acquire' nor 'release' has been provided, just write the data
-      case _ => Some(kv.copy(modifyIndex = nextModificationIndex, session = None))
-    }    
-
-    res.foreach{ kv => 
-      keyValues.put(kv.key, kv)
-      logger.debug(s"Storing key/value [$kv]")
-      //notify any lock holders that the key has changed
-      //we don't care to prune used Semaphores, sure this will leak objects but for a test rig it won't matter.
-      //only release those that have a 'index' <= than the ModifyIndex on the key
-      blockers.get(kv.key).foreach(_.foreach(_.releaseIfIndexReached(kv.modifyIndex)))
-    }
-    res.isDefined
-  }
-
-  private def readKey(key:String, index:Int, wait:FiniteDuration):Option[KeyValue] = {
-    keyValues.get(key).flatMap{kv =>
-      if(index <= kv.modifyIndex) 
-        Some(kv)
-      else {
-        val semaphore = new java.util.concurrent.Semaphore(0)
-        val blocker = Seq(Blocker(index, semaphore))
-        val seq = blockers.get(kv.key) map(_ ++ blocker) getOrElse blocker
-        blockers.put(kv.key, seq)
-        logger.debug(s"Found key [$key] but index is [${kv.modifyIndex}], adding blocker on index [$index] with wait [${wait.toSeconds}]s")
-        //hold here until the time runs out of someone updates the key
-        semaphore.tryAcquire(wait.toMillis, TimeUnit.MILLISECONDS) 
-        readKey(key, 0, zeroDuration)
-      }
-    }
-  }
-
 }
 
 
